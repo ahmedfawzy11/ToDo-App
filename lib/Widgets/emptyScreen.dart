0d8e0d2ea130ff7bc93fcb82bd60bffb74597a56
AHMED FAWZY <ahmedmido_fawzy@yahoo.com>
2022-07-28 10:51:30 +0200

    Update emptyScreen.dart

--- lib/Widgets/emptyScreen.dart
+++ lib/Widgets/emptyScreen.dart
@@ -22,27 +22,6 @@ class EmptyScreen extends StatelessWidget {
           const SizedBox(
             height: 60,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              BounceInDown(
-                duration: const Duration(seconds: 3),
-                child: Square(),
-              ),
-              BounceInUp(
-                duration: const Duration(seconds: 3),
-                child: Square(),
-              ),
-              BounceInLeft(
-                duration: const Duration(seconds: 3),
-                child: Square(),
-              ),
-              BounceInRight(
-                duration: const Duration(seconds: 3),
-                child: Square(),
-              ),
-            ],
-          ),
         ],
       ),
     );
